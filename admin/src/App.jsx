50621853d963f693718318572ffaefe9247a9bb0
Saugata Ghosh <135034312+Saugataghosh24@users.noreply.github.com>
2024-09-29 11:40:10 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import List from './pages/List/List'
 import Add from './pages/Add/Add'
 import Orders from './pages/Orders/Orders'
+import Login from './pages/Login/Login'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,9 +15,13 @@ const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // Redirect to Orders page only if the current route is '/'
   useEffect(() => {
-    if (location.pathname === '/') {
+    // Check if admin is logged in, if not, redirect to login
+    const token = localStorage.getItem('admin_token');
+    if (!token && location.pathname !== '/login') {
+      navigate('/login');
+    }
+    else if (token && location.pathname === '/') {
       navigate('/orders');
     }
   }, [location, navigate]);
@@ -33,6 +38,7 @@ const App = () => {
           <Route path="/add" element={<Add url={url}/>} />
           <Route path="/list" element={<List url={url}/>} />
           <Route path="/orders" element={<Orders url={url}/>} />
+          <Route path="/login" element={<Login url={url} />} />
         </Routes>
       </div>
     </div>
